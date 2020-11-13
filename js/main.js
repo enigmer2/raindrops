65194ccaa2f868fae1e09e3c693dd5cc16013e4e
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-11-13 15:11:50 +0300

    feat: canvas land & animate

--- js/main.js
+++ js/main.js
@@ -3,26 +3,12 @@ let speed = 0;  // скорость капель
 let level = 0;  // уровень
 let op1 = 0;    // первый операнд
 let op2 = 0;    // второй операнд
-
-function randomInteger(min, max) {
-    // случайное число от min до (max+1)
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
-
-
+let isPlayed = false; // игра запущена или нет
+let isMuted = false; // звук выключен или нет
 let elem = document.documentElement;
-
-function openFullscreen() {
-  elem.requestFullscreen();
-}
-function closeFullscreen() {
-  document.exitFullscreen();
-}
-
-console.clear();
-let canvas = document.querySelector('canvas');
+let canvas = document.getElementById('land');
 let canvasWraper = document.getElementById('canvas');
+let isFullScreen = false;
 let c = canvas.getContext('2d');
 canvas.height = innerHeight;
 canvas.width = canvasWraper.offsetWidth;
@@ -35,53 +21,92 @@ increment1 = frequency;
 increment2 = frequency;
 w = 0.5;
 
-function animate(){
+function play(idAudio) { //воспроизводит по id аудио
+	let x = document.getElementById(idAudio);
+	x.play();
+}
 
-	c.clearRect(0,0,cw,ch);
-    requestAnimationFrame(animate);
+function pause(idAudio) { //ставить на паузу по id аудио
+	let x = document.getElementById(idAudio);
+	x.pause();
+}
 
-    var img = new Image();   // Создаём новый объект Image
-    img.src = 'media/svg-2346025.svg'; // Устанавливаем путь к источнику
-    c.drawImage(img, innerWidth/10, canvas.height/2, 600, 606); //рисуем картинку в канвас
-    c.drawImage(img, -(innerWidth/2), innerWidth/0.85, 2000, 1006); //рисуем картинку в канвас
+function randomInteger(min, max) {    // случайное число от min до (max+1)
+	let rand = min + Math.random() * (max + 1 - min);
+	return Math.floor(rand);
+}
 
-    var img = new Image();   // Создаём новый объект Image
-    img.src = '/media/Bunny.svg'; // Устанавливаем путь к источнику
-    c.drawImage(img, innerWidth/5.4, canvas.height/2.1, 300, 300); //рисуем картинку в канвас
+function openFullscreen() { // открывает на fullscreen
+	elem.requestFullscreen();
+	isFullScreen = true;
+}
 
-    c.beginPath();
-	c.moveTo(0,ch/2);
-	for(let x = 0; x < cw; x++){
-		for(let y = 7; y < 20; y++){
-			c.lineTo(x, y * 60 + ch/2 + Math.sin(x * frequency + increment2) * amplitude * Math.sin(w));
-		}
-	}
-	c.stroke();
-	c.strokeStyle = 'rgba(0,0,155, 0.5)';
-    c.closePath();
+function closeFullscreen() { // сворачивает fullscreen
+	document.exitFullscreen();
+	isFullScreen = false;
+}
+
+
+
+function land() {
+	c.clearRect(0, 0, cw, ch);
+	requestAnimationFrame(land);
+
+	var img = new Image();   // Создаём новый объект Image
+	img.src = 'media/svg-2346025.svg'; // Устанавливаем путь к источнику
+	c.drawImage(img, innerWidth / 10, canvas.height / 2, 600, 606); //рисуем картинку в канвас
+	c.drawImage(img, -(innerWidth / 2), innerWidth / 0.85, 2000, 1006); //рисуем картинку в канвас
+
+	var img = new Image();   // Создаём новый объект Image
+	img.src = 'media/Bunny.svg'; // Устанавливаем путь к источнику
+	c.drawImage(img, innerWidth / 5.4, canvas.height / 2.1, 300, 300); //рисуем картинку в канвас
+	
+}
+
+function animateWave() {
+	requestAnimationFrame(animateWave);
 
 	c.beginPath();
-	c.moveTo(0,ch/2);
-	for(let x = 0; x < cw; x++){
-		for(let y = 8; y < 20; y++){
-			c.lineTo(x, y * 60 + ch/2 + Math.sin(x * frequency + increment1) * amplitude * Math.sin(w));
+	c.moveTo(0, ch / 2);
+	for (let x = 0; x < cw; x++) {
+		for (let y = 7; y < 20; y++) {
+			c.lineTo(x, y * 60 + ch / 2 + Math.sin(x * frequency + increment2) * amplitude * Math.sin(w));
 		}
 	}
 	c.stroke();
 	c.strokeStyle = 'rgba(0,0,155, 0.5)';
-    c.closePath();
-    
-	increment1+= 0.05;
-	increment2 -= 0.03;
-    w+=0.05
-     
-}
-animate();
+	c.closePath();
 
+	c.beginPath();
+	c.moveTo(0, ch / 2);
+	for (let x = 0; x < cw; x++) {
+		for (let y = 8; y < 20; y++) {
+			c.lineTo(x, y * 60 + ch / 2 + Math.sin(x * frequency + increment1) * amplitude * Math.sin(w));
+		}
+	}
+	c.stroke();
+	c.strokeStyle = 'rgba(0,0,155, 0.5)';
+	c.closePath();
+
+	increment1 += 0.05;
+	increment2 -= 0.03;
+	w += 0.05
+
+}
+function rainDrop() {
+	requestAnimationFrame(rainDrop);
+	
+}
+
+land();
+animateWave();
+rainDrop();
 
 addEventListener('resize', () => {
-    canvas.height = canvasWraper.offsetHeight;
-    canvas.width = canvasWraper.offsetWidth;
+	canvas.height = canvasWraper.offsetHeight;
+	canvas.width = canvasWraper.offsetWidth;
 	ch = canvas.height;
 	cw = canvas.width;
 });
+
+console.clear();
